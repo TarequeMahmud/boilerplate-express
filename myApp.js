04703b79c1c835f7d1ffabd8ac87729c378ab76c
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-09-23 14:28:45 +0600

    Using middleware to modify the request params

--- myApp.js
+++ myApp.js
@@ -20,4 +20,15 @@ app.get("/json", function (req, res) {
   });
 });
 
+pp.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.json({ time: req.time });
+  }
+);
+
 module.exports = app;
